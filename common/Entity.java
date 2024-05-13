90f8a0b08d2ce9367c25f8f9403eda1671a69dc1
hellopir2 <104927802+hellopir2@users.noreply.github.com>
2024-05-13 20:50:26 +0000

    entity class

---
+++ common/Entity.java
@@ -0,0 +1,35 @@
+public class Entity implements Collideable<Entity>{
+    public float x;
+    public float y;
+    public float angle;
+    public float dx;
+    public float dy;
+    public float r;
+
+    public Entity(float x, float y, float r){
+        setPos(x, y);
+        this.r = r;
+        this.angle = 0;
+        setVel(0, 0);
+    }
+    
+    public Entity(float x, float y, float r, float angle){
+        setPos(x, y);
+        this.r = r;
+        this.angle = angle;
+        setVel(0, 0);
+    }
+
+    public void setPos(float x, float y){
+        this.x = x;
+        this.y = y;
+    }
+    
+    public void setVel(float dx, float dy){
+        this.dx = dx;
+        this.dy = dy;
+    }
+
+    public void didCollide(Entity other){}
+
+}
