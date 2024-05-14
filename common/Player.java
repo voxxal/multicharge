1f7f71e240545e2575454a23ee07ad2a191879de
hellopir2 <104927802+hellopir2@users.noreply.github.com>
2024-05-14 20:16:32 +0000

    fix errors

--- common/Player.java
+++ common/Player.java
@@ -16,9 +16,9 @@ public class Player extends Entity {
     }
 
     public void draw() {
-        DrawCircleV(pos.toRaylib(), radius, new Color(255, 145, 145, 255));
-        DrawCircleLinesV(pos.toRayLib(), radius, new Color(10, 10, 10, 255));
-        DrawRectanglePro(new Rectangle(pos.x - 10, pos.y - radius + 4, 8, 40), new Vector2(pos.x, pos.y), angle, new Color(140, 136, 126, 255));
+        DrawCircleV(pos.toRaylib(), radius, new Color().r((byte)255).b((byte)145).g((byte)145).a((byte)255));
+        DrawCircleLinesV(pos.toRaylib(), radius, new Color().r((byte)10).b((byte)10).g((byte)10).a((byte)255));
+        DrawRectanglePro(new Rectangle().x(pos.x - 10).y(pos.y - radius + 4).width(40).height(8), new Vector2().x(pos.x).y(pos.y), angle, new Color().r((byte)140).b((byte)136).g((byte)126).a((byte)255));
     }
 
     public String toString() {
