5c801d461911ea3a88ea3ccf6fcbf9e83ee00dcf
hellopir2 <104927802+hellopir2@users.noreply.github.com>
2024-05-14 20:48:10 +0000

    HOPEFULLY FIX RECTANGLE ROTATION

--- common/Player.java
+++ common/Player.java
@@ -18,9 +18,7 @@ public class Player extends Entity {
     public void draw() {
         DrawCircleV(pos.toRaylib(), radius, new Color().r((byte)10).b((byte)10).g((byte)10).a((byte)255));
         DrawCircleV(pos.toRaylib(), radius-3, new Color().r((byte)255).b((byte)145).g((byte)145).a((byte)255));
-        DrawRectanglePro(new Rectangle().x(pos.x - 10).y(pos.y - radius + 4).width(40).height(8), new Vector2().x(pos.x).y(pos.y), angle, new Color().r((byte)140).b((byte)136).g((byte)126).a((byte)255));
-        DrawRectangleRec(new Rectangle().x(pos.x - 10).y(pos.y - radius + 4).width(40).height(8), new Color().r((byte)0).b((byte)136).g((byte)0).a((byte)255));
-        DrawRectangle(pos.x - 10, pos.y - radius + 4, 40, 8, new Color().r((byte)0).b((byte)0).g((byte)255).a((byte)255));
+        DrawRectanglePro(new Rectangle().x(pos.x-10).y(pos.y-radius + 4).width(40).height(8), new Vector2().x(-10).y(-radius + 4), angle, new Color().r((byte)140).b((byte)136).g((byte)126).a((byte)255));
     }
 
     public String toString() {
