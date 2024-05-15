f43b490767672673a5c974f6714c75e8bd3b9173
voxal <aidenhohoho@gmail.com>
2024-05-15 11:23:07 -0700

    ak47

--- common/Weapon.java
+++ common/Weapon.java
@@ -2,12 +2,15 @@ import java.io.Serializable;
 
 public abstract class Weapon implements Serializable {
 
+    public String name;
     public boolean reloading;
     public float reloadSpeed;
     public float reloadTimer;
     public int maxAmmo;
     public int ammo;
     public boolean magazined;
+    public float shotCooldown;
+    public float shotTimer;
 
     public abstract boolean shoot(World world, Player player);
 
@@ -33,21 +36,35 @@ public abstract class Weapon implements Serializable {
                 }
             }
         }
+        shotTimer -= dt;
+    }
+
+    public String toString() {
+        return String.format(
+            "%s - %d | %d %s",
+            name,
+            ammo,
+            maxAmmo,
+            reloading ? String.format("Reloading %.2fs", reloadTimer) : ""
+        );
     }
 
     public static class Remington870 extends Weapon {
 
         public Remington870() {
+            name = "Remington 870";
             reloadSpeed = 0.5f;
             maxAmmo = 5;
             ammo = 5;
             magazined = false;
+            shotCooldown = 0.5f;
         }
 
         public void draw(Player player) {}
 
         public boolean shoot(World world, Player player) {
-            if (ammo > 0) {
+            if (ammo > 0 && shotTimer <= 0) {
+                reloading = false;
                 ammo--;
                 for (int i = 0; i < 8; i++) {
                     world.add(
@@ -59,19 +76,45 @@ public abstract class Weapon implements Serializable {
                         )
                     );
                 }
-            } else {
+                shotTimer = shotCooldown;
+            } else if (ammo <= 0) {
                 reload();
             }
             return false;
         }
     }
 
-    public String toString() {
-        return String.format(
-            "Remington 870 - %d | %d %s",
-            ammo,
-            maxAmmo,
-            reloading ? String.format("Reloading %.2fs", reloadTimer) : ""
-        );
+    public static class Ak47 extends Weapon {
+
+        public Ak47() {
+            name = "AK-47";
+            reloadSpeed = 3f;
+            maxAmmo = 30;
+            ammo = 30;
+            magazined = true;
+            shotCooldown = 0.1f;
+        }
+
+        public void draw(Player player) {}
+
+        public boolean shoot(World world, Player player) {
+            if (ammo > 0 && shotTimer <= 0) {
+                reloading = false;
+                ammo--;
+                world.add(
+                    new Bullet(
+                        player.pos,
+                        5,
+                        player.angle + (float) Math.random() / 8,
+                        500
+                    )
+                );
+                shotTimer = shotCooldown;
+            } else if (ammo <= 0) {
+                reload();
+                return false;
+            }
+            return true;
+        }
     }
 }
