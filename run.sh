54e874ab7932609f0316b79089a0930ef00183df
voxal <aidenhohoho@gmail.com>
2025-09-15 19:18:20 -0700

    archiving

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 set -e
 
-javac -d target/common -cp jars/* common/*.java
-javac -d target/server -cp jars/*:target/common server/*.java
-javac -d target/client -cp jars/*:target/common client/*.java
+#javac -d target/common -cp jars/* common/*.java
+#javac -d target/server -cp jars/*:target/common server/*.java
+#javac -d target/client -cp jars/*:target/common client/*.java
 java -cp target/server:jars/*:target/common Server &
 # SERV_PID = $!
 java -cp target/client:jars/*:target/common Client &
